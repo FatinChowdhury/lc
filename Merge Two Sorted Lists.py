2e3d67def988afda7933d8a0a28c8995387233e1
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-05-19 00:44:28 -0400

    Update Merge Two Sorted Lists.py

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -3,32 +3,22 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-# Iterative
 class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        dummy = node = ListNode()
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                node.next = list1
-                list1 = list1.next
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
             else:
-                node.next = list2
-                list2 = list2.next
-            node = node.next
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
 
-        node.next = list1 or list2
-
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
         return dummy.next
-    
-# Recursive
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        lil, big = (list1, list2) if list1.val < list2.val else (list2, list1)
-        lil.next = self.mergeTwoLists(lil.next, big)
-        return lil
